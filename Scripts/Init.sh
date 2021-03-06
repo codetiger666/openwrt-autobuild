ae6f29025d2fb125263e35d790cbbff18566c585
codetiger <nnweb365@gmail.com>
2021-03-06 12:55:54 +0800

    add some config

--- Scripts/Init.sh
+++ Scripts/Init.sh
@@ -16,17 +16,20 @@ Diy-Part1() {
     Date=`date "+%Y/%m/%d"`
     mkdir -p $GITHUB_WORKSPACE/openwrt/package/base-files/files/etc/config > /dev/null 2>1
     cd $GITHUB_WORKSPACE
+    /usr/bin/cp /usr/bin/upx openwrt/staging_dir/host/bin
+    /usr/bin/cp $GITHUB_WORKSPACE/openwrt/package/base-files/files/etc/banner $GITHUB_WORKSPACE/openwrt/package/base-files/files/etc/banner.back
     mv -f Customize/banner $GITHUB_WORKSPACE/openwrt/package/base-files/files/etc
     cd $GITHUB_WORKSPACE/openwrt/package/base-files/files/bin
+    /usr/bin/cp config_generate config_generate.back
     sed -i "s/192.168.1.1/10.10.1.1/g" config_generate
     sed -i "s/192.168/10.10/g" config_generate
     mkdir -p $GITHUB_WORKSPACE/openwrt/package/base-files/files/etc/config > /dev/null 2>1
     mkdir -p  $GITHUB_WORKSPACE/openwrt/package/CodeTiger > /dev/null 2>1
     cd $GITHUB_WORKSPACE/openwrt/package/CodeTiger
-
+    svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/redsocks2
     git clone https://github.com/jerrykuku/luci-theme-argon.git
     git clone https://github.com/jerrykuku/luci-app-argon-config.git
-    
+    /usr/bin/cp $GITHUB_WORKSPACE/openwrt/package/base-files/files/etc/openwrt_release $GITHUB_WORKSPACE/openwrt/package/base-files/files/etc/openwrt_release.back
     sed -i '/DISTRIB_REVISION/d' $GITHUB_WORKSPACE/openwrt/package/base-files/files/etc/openwrt_release
     echo "DISTRIB_REVISION='codetiger 0.1 [$Date]'" >> $GITHUB_WORKSPACE/openwrt/package/base-files/files/etc/openwrt_release
     sed -i '/DISTRIB_DESCRIPTION/d' $GITHUB_WORKSPACE/openwrt/package/base-files/files/etc/openwrt_release
@@ -38,10 +41,12 @@ Diy-Part1() {
     if [ "$INCLUDE_SmartDNS" == "true" ]; then
         cd $GITHUB_WORKSPACE/openwrt/package/CodeTiger
         git clone https://github.com/pymumu/luci-app-smartdns.git
+        /usr/bin/cp Customize/smartdns $GITHUB_WORKSPACE/openwrt/package/base-files/files/etc/config
     fi
     if [ "$INCLUDE_Passwall" == "true" ]; then
         cd $GITHUB_WORKSPACE/openwrt/package/CodeTiger
         git clone https://github.com/xiaorouji/openwrt-passwall.git -b main
+        /usr/bin/cp Customize/passwall $GITHUB_WORKSPACE/openwrt/package/base-files/files/etc/config
     fi
     if [ "$INCLUDE_VSSR" == "true" ]; then
         cd $GITHUB_WORKSPACE/openwrt/package/CodeTiger
@@ -58,8 +63,8 @@ Diy-Part1() {
 Diy-Part2_x86_64() {
     Date=`date "+%Y%m%d"`
 	mkdir bin/Firmware
-	mv -f bin/targets/x86/64/openwrt-x86-64-*-combined-efi.img.gz bin/Firmware/"openwrt-x86-64-efi-$Date.img.gz"
-    mv -f bin/targets/x86/64/openwrt-x86-64-*-combined.img.gz bin/Firmware/"openwrt-x86-64-$Date.img.gz"
+	mv -f bin/targets/x86/64/openwrt-x86-64-generic-ext4-combined-efi.img.gz bin/Firmware/"openwrt-x86-64-efi-$Date.img.gz"
+    mv -f bin/targets/x86/64/openwrt-x86-64-generic-ext4-combined.img.gz bin/Firmware/"openwrt-x86-64-$Date.img.gz"
 	_MD5_efi=$(md5sum bin/Firmware/"openwrt-x86-64-efi-$Date.img.gz" | cut -d ' ' -f1)
     _MD5=$(md5sum bin/Firmware/"openwrt-x86-64-$Date.img.gz" | cut -d ' ' -f1)
 	_SHA256_efi=$(sha256sum bin/Firmware/"openwrt-x86-64-efi-$Date.img.gz" | cut -d ' ' -f1)
